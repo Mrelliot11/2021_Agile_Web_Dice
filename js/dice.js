9c0430bba33dae9f21ddad000fc5b4c77f9391d0
Elliot Dubrow <emn2345@gmail.com>
2021-09-29 19:38:27 -0500

    [Finishes #179680877] Major rolldice changes

--- js/dice.js
+++ js/dice.js
@@ -1,87 +1,53 @@
-// Determine variables
-var rollNumber = 1;
-var diceSize = 6; 
-var firstName = "";
-var rollResult = 0;
-var resultSum = 0;
-var message = "";
-var index = 1;
-
-
 function rollDice() {
-    //Check to see if roll amounis a valid number
-    while (numberValidator(rollNumber)){
-    
-        alert("The number you have entered for number of rolls is not valid please enter a valid positive number.");
-    }
 
-    //Check to see if dice size is a valid number
-    while (numberValidator(diceSize)){
-    
-        alert("The number you have entered for size of dice is not valid please enter a valid positive number.");
-    }
+  // Determine variables
+  var diceCount;
+  var diceSize = 0;
+  var rollResult = 0;
+  var resultSum = 0;
+  var message = "";
+  var index = 1;
 
+  document.getElementById('diceRolls').innerHTML = "";
+
+
+  var rollCountObj = document.getElementById('numberOfDiceInput');
+  var messageResultTextObj = document.getElementById('messageResultText');
+
+  var rollNumberObj = document.getElementById('diceSizeList');
+
+  diceCount = Number(rollCountObj.value);
+  diceSize = Number(rollNumberObj.value);
+  console.log(diceCount);
+
+
+  if (Number.isInteger(diceCount)) {
     //Repeats while statement based on number selected in inputNumber
-    while (index <= rollNumber) {
-    //Selects random number between 1 and 6
-    rollResult = Math.floor(Math.random() * diceSize) + 1;
-    //Displays result of each roll by creating <p> element for each roll
-    var tagP = document.createElement("p");
-    var resultOutput = document.createTextNode("Roll #" + index + ": " + rollResult);
-    var divElement = document.getElementById("diceRolls")
-    
-    tagP.appendChild(resultOutput);
-    divElement.appendChild(tagP);
-    
-    //Adds the result of each roll to the sum
-    resultSum += rollResult;
-    //add 1 to index
-    index++;
+    while (index <= diceCount) {
+      //Selects random number between 1 and 6
+      rollResult = Math.floor(Math.random() * diceSize) + 1;
+      //Displays result of each roll by creating <p> element for each roll
+      var tagP = document.createElement("p");
+      var resultOutput = document.createTextNode("Roll #" + index + ": " + rollResult);
+      var divElement = document.getElementById("diceRolls");
+
+      tagP.appendChild(resultOutput);
+      divElement.appendChild(tagP);
+
+      //Adds the result of each roll to the sum
+      resultSum = resultSum + rollResult;
+      //add 1 to index
+      index++;
+      console.log(resultSum);
+      //Displays sum of all rolls
+      message = "The total of all dice rolled is: " + resultSum;
+
+      document.getElementById('messageResultText').innerHTML = message;
     }
-    //Displays sum of all rolls
-    message = "The total of all dice rolled is: " + resultSum;
+  } else {
 
-    document.getElementById('messageResultText').innerHTML = message;
+    console.log(diceCount);
+    messageResultTextObj.innerHTML = "Please enter an integer value e.g. 1, 2, 3 \n Other types of numbers are not accepted";
 
+  }
 }
-
-
-
-//Function for number validation
-function numberValidator(vNumber) {
-  var validNumber = false;
-  //Converts input number to a number
-  vNumber = Number(vNumber);
-  //Check if it is a number
-  if (isNaN(vNumber)){
-    validNumber = true;
-  }
-  //Check if input number is an integer
-  if (parseFloat(vNumber) != parseInt(vNumber)) {
-    validNumber = true;
-  }
-  // Check to see if it is a positive number
-  if (vNumber <= 0) {
-    validNumber = true;
-  }
-  
-  return validNumber;
-}
-
-// Gets the values for number of times to roll dice
-function getRollNumber(objNumberDropDown) { 
-    console.log(objNumberDropDown); 
-    var rollNumberList = document.getElementById("rollNumberList");  
-    rollNumberList.value = objNumberDropDown.value;
-    rollNumber = rollNumberList.value;
-
-    }  
-
-// Gets the values for the size of the Dice
-function getDiceSize(objDiceDropDown) { 
-    console.log(objDiceDropDown); 
-    var diceSizeList = document.getElementById("diceSizeList");  
-    diceSizeList.value = objDiceDropDown.value;
-    diceSize = diceSizeList.value;
-    }  
-
