ada4089dc4a3f3e3fce84b32d2cd8e5fab0d5ba8
Jacob Frideres <jakefrideres@gmail.com>
2021-09-30 12:08:40 -0500

    [Finishes #179680881] Allowing user to add min/max to dice rolls.

--- js/dice.js
+++ js/dice.js
@@ -7,6 +7,8 @@ function rollDice() {
   var resultSum = 0;
   var message = "";
   var index = 1;
+  var rollMinResult = 0;
+  var rollMaxResult = 0;
 
   document.getElementById('diceRolls').innerHTML = "";
 
@@ -14,17 +16,31 @@ function rollDice() {
   var rollCountObj = document.getElementById('numberOfDiceInput');
   var rollNumberObj = document.getElementById('diceSizeList');
 
+  //Create variables to get min and max values from the user
+  var rollMin = document.getElementById('diceRollMin');
+  var rollMax = document.getElementById('diceRollMax');
+
   //Create object of the message result text
   var messageResultTextObj = document.getElementById('messageResultText');
   //Set dice count and size as numbers
   diceCount = Number(rollCountObj.value);
   diceSize = Number(rollNumberObj.value);
+  //Set roll min and max as numbers
+  rollMinResult = Number(rollMin.value);
+  rollMaxResult = Number(rollMax.value);
 
   if (Number.isInteger(diceCount) && diceCount > 0) {
     //Repeats while statement based on number selected in inputNumber
     while (index <= diceCount) {
       //Selects random number between 1 and 6
       rollResult = Math.floor(Math.random() * diceSize) + 1;
+      //Set rollResult to the max/min that the user has chosen
+      if (rollResult < rollMinResult) {
+          rollResult = rollMinResult
+      }
+      if (rollResult > rollMaxResult) {
+          rollResult = rollMaxResult
+      }
       //Displays result of each roll by creating <p> element for each roll
       var tagP = document.createElement("p");
       var resultOutput = document.createTextNode("Roll #" + index + ": " + rollResult);
