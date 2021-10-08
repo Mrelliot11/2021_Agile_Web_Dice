5f46eebfe123b017eec3a0813eb22c3d175e160a
Elliot Dubrow <emn2345@gmail.com>
2021-10-08 00:05:45 -0500

    fixed js typos

--- js/dice.js
+++ js/dice.js
@@ -115,20 +115,20 @@ function rollDice() {
 
 function userLogin() {
    
-  var username = ""
-  var password = ""
-  var usernameLength = ""
-  var passwordLength = ""
+  var username = "";
+  var password = "";
+  var usernameLength = "";
+  var passwordLength = "";
  
-  var username = document.getElementById("userUsername");
-  var password = document.getElementById("userPassword");
+  username = document.getElementById("userUsername");
+  password = document.getElementById("userPassword");
  
-  usernameLength = username.value.length;
-  passwordLength = password.value.length;
+  usernameLength = username.length;
+  passwordLength = password.length;
  
   if (usernameLength >= 12 && passwordLength >= 12) {
-      window.location="index.html" 
+      window.location="index.html";
    }   else {
-       alert("Please make sure that the username and password you have entered are at least 12 characters long.")
+       alert("Please make sure that the username and password you have entered are at least 12 characters long.");
    }
  }
