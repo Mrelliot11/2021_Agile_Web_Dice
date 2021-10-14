a7824a27ee4df1f1c549e564b734d6a615532c8c
Elliot Dubrow <emn2345@gmail.com>
2021-10-14 12:01:23 -0500

    moved login function

--- js/dice.js
+++ js/dice.js
@@ -147,24 +147,5 @@ function rollDice() {
   }
 }
 
-function userLogin() {
-   
-  var username = "";
-  var password = "";
-  var usernameLength = "";
-  var passwordLength = "";
- 
-  username = document.getElementById("userUsername");
-  password = document.getElementById("userPassword");
- 
-  usernameLength = username.length;
-  passwordLength = password.length;
- 
-  if (usernameLength >= 12 && passwordLength >= 12) {
-      window.location="index.html";
-   }   else {
-       alert("Please make sure that the username and password you have entered are at least 12 characters long.");
-   }
- }
 
 
