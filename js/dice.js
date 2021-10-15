6c2bbd8fc20b82861259a82d8779838ff3809661
alillo2 <alillo2@student.cvtc.edu>
2021-10-14 21:42:40 -0500

    [Finishes #179945988] Adding delay for each roll when roll number > 20

--- js/dice.js
+++ js/dice.js
@@ -7,7 +7,7 @@ var rollSound = new Audio();
 volume = document.getElementById("volume-control");
 volume.addEventListener("change", function(e) {
 rollSound.volume = e.currentTarget.value / 100;
-})
+});
 
 function rollDice() {
     "use strict";
@@ -66,81 +66,98 @@ function rollDice() {
     if (Number.isInteger(diceCount) && diceCount > 0) {
         //Repeats while statement based on number selected in inputNumber
         while (index <= diceCount) {
-            //Selects random number between 1 and 6 and adds en
-            rollResult = (Math.floor(Math.random() * diceSize) + 1) + rollEnhancement;
+            
+            // If dice count is 20 or lower do a delayed roll otherwise output all data instantly
+            if (diceCount <= 20) {
+                delayedRoll(index);
+                //add 1 to index
+                index = index + 1;
+            } else if (diceCount > 20){
+                //Selects random number between 1 and 6 and adds en
+                rollResult = (Math.floor(Math.random() * diceSize) + 1) + rollEnhancement;
 
-            //Displays result of each roll by creating <p> element for each roll
-            tagP = document.createElement("p");
-            resultOutput = document.createTextNode("Roll #" + index + ": " + rollResult);
-            divElement = document.getElementById("diceRolls");
-            tagP.appendChild(resultOutput);
-            divElement.appendChild(tagP);
+                //Displays result of each roll by creating <p> element for each roll
+                tagP = document.createElement("p");
+                resultOutput = document.createTextNode("Roll #" + index + ": " + rollResult);
+                divElement = document.getElementById("diceRolls");
+                tagP.appendChild(resultOutput);
+                divElement.appendChild(tagP);
 
-            //Adds the result of each roll group to the sum
-            resultSum = resultSum + rollResult;
+                //Adds the result of each roll group to the sum
+                resultSum = resultSum + rollResult;
 
-            //add 1 to index
-            index = index + 1;
+                //add 1 to index
+                index = index + 1;
 
-            //Displays sum of all rolls
-            message = "The total of this roll is: " + resultSum;
 
-            //Displays max of all rolls
-            resultMax = ((diceCount * diceSize) + (diceCount * rollEnhancement));
-            messageMax = "The max of all dice rolled is: " + resultMax;
+                //Displays sum of all rolls
+                message = "The total of this roll is: " + resultSum;
 
-            //Displays min of all rolls
-            resultMin = (diceCount + (diceCount * rollEnhancement));
-            messageMin = "The min of all dice rolled is: " + resultMin;
-            //Adds sum of each roll group to overall sum
-            totalSum = totalSum + rollResult;
+                //Displays max of all rolls
+                resultMax = ((diceCount * diceSize) + (diceCount * rollEnhancement));
+                messageMax = "The max of all dice rolled is: " + resultMax;
 
-            //Displays average of all rolls
-            resultAverage = Math.round(resultSum / diceCount);
-            messageAverage = "The average of all dice rolled is: " + resultAverage;
+                //Displays min of all rolls
+                resultMin = (diceCount + (diceCount * rollEnhancement));
+                messageMin = "The min of all dice rolled is: " + resultMin;
+                //Adds sum of each roll group to overall sum
+                totalSum = totalSum + rollResult;
 
-            //Displays overall sum of all rolls
-            messageTotal = "The overall sum of all rolls is: " + totalSum;
-            document.getElementById("messageAllRollsText").innerHTML = messageTotal;
+                //Displays average of all rolls
+                resultAverage = Math.round(resultSum / diceCount);
+                messageAverage = "The average of all dice rolled is: " + resultAverage;
 
-            //Displays each calculation
-            document.getElementById("messageResultText").innerHTML = message;
-            document.getElementById("messageAverageResultText").innerHTML = messageAverage;
-            document.getElementById("messageMaxResultText").innerHTML = messageMax;
-            document.getElementById("messageMinResultText").innerHTML = messageMin;
+                //Displays overall sum of all rolls
+                messageTotal = "The overall sum of all rolls is: " + totalSum;
+                document.getElementById("messageAllRollsText").innerHTML = messageTotal;
 
-            // sets the dice color based on the User's selection
-            if (diceColor === "white") {
-            //Show die depending on the size the user chooses
-                if (diceSize === 4) {
+                //Displays each calculation
+                document.getElementById("messageResultText").innerHTML = message;
+                document.getElementById("messageAverageResultText").innerHTML = messageAverage;
+                document.getElementById("messageMaxResultText").innerHTML = messageMax;
+                document.getElementById("messageMinResultText").innerHTML = messageMin;
+
+                // sets the dice color based on the User's selection
+                if (diceColor === "white") {
+                    //Show die depending on the size the user chooses
+                    if (diceSize === 4) {
                     document.getElementById("d4ImageLight").style.visibility = "visible";
-                } else if (diceSize === 6) {
+                    } else if (diceSize === 6) {
                     document.getElementById("d6ImageLight").style.visibility = "visible";
-                } else if (diceSize === 8) {
+                    } else if (diceSize === 8) {
                     document.getElementById("d8ImageLight").style.visibility = "visible";
-                } else if (diceSize === 10) {
+                    } else if (diceSize === 10) {
                     document.getElementById("d10ImageLight").style.visibility = "visible";
-                } else if (diceSize === 12) {
+                    } else if (diceSize === 12) {
                     document.getElementById("d12ImageLight").style.visibility = "visible";
-                } else if (diceSize === 20) {
+                    } else if (diceSize === 20) {
                     document.getElementById("d20ImageLight").style.visibility = "visible";
-                }
-            } else if (diceColor === "black") {
-                //Show die depending on the size the user chooses
-                if (diceSize === 4) {
+                    }
+                } else if (diceColor === "black") {
+                    //Show die depending on the size the user chooses
+                    if (diceSize === 4) {
                     document.getElementById("d4ImageDark").style.visibility = "visible";
-                } else if (diceSize === 6) {
+                    } else if (diceSize === 6) {
                     document.getElementById("d6ImageDark").style.visibility = "visible";
-                } else if (diceSize === 8) {
+                    } else if (diceSize === 8) {
                     document.getElementById("d8ImageDark").style.visibility = "visible";
-                } else if (diceSize === 10) {
+                    } else if (diceSize === 10) {
                     document.getElementById("d10ImageDark").style.visibility = "visible";
-                } else if (diceSize === 12) {
+                    } else if (diceSize === 12) {
                     document.getElementById("d12ImageDark").style.visibility = "visible";
-                } else if (diceSize === 20) {
+                    } else if (diceSize === 20) {
                     document.getElementById("d20ImageDark").style.visibility = "visible";
+                    }
                 }
+
+
+
             }
+
+
+
+
+
         }
     } else {
         //Focuses mouse to text box
@@ -148,6 +165,89 @@ function rollDice() {
         messageResultTextObj.innerHTML = "Please enter a positive integer value e.g. 1, 2, 3 \n Other types of numbers are not accepted";
 
     }
+// function to delay rolling by 2 seconds each roll
+function delayedRoll(i) {
+    setTimeout(function() {
+        //Selects random number between 1 and 6 and adds en
+        rollResult = (Math.floor(Math.random() * diceSize) + 1) + rollEnhancement;
+
+        //Displays result of each roll by creating <p> element for each roll
+        tagP = document.createElement("p");
+        resultOutput = document.createTextNode("Roll #" + i + ": " + rollResult);
+        divElement = document.getElementById("diceRolls");
+        tagP.appendChild(resultOutput);
+        divElement.appendChild(tagP);
+
+        //Adds the result of each roll group to the sum
+        resultSum = resultSum + rollResult;
+
+
+
+
+        //Displays sum of all rolls
+        message = "The total of this roll is: " + resultSum;
+
+        //Displays max of all rolls
+        resultMax = ((diceCount * diceSize) + (diceCount * rollEnhancement));
+        messageMax = "The max of all dice rolled is: " + resultMax;
+
+        //Displays min of all rolls
+        resultMin = (diceCount + (diceCount * rollEnhancement));
+        messageMin = "The min of all dice rolled is: " + resultMin;
+        //Adds sum of each roll group to overall sum
+        totalSum = totalSum + rollResult;
+
+        //Displays average of all rolls
+        resultAverage = Math.round(resultSum / diceCount);
+        messageAverage = "The average of all dice rolled is: " + resultAverage;
+
+        //Displays overall sum of all rolls
+        messageTotal = "The overall sum of all rolls is: " + totalSum;
+        document.getElementById("messageAllRollsText").innerHTML = messageTotal;
+
+        //Displays each calculation
+        document.getElementById("messageResultText").innerHTML = message;
+        document.getElementById("messageAverageResultText").innerHTML = messageAverage;
+        document.getElementById("messageMaxResultText").innerHTML = messageMax;
+        document.getElementById("messageMinResultText").innerHTML = messageMin;
+
+        // sets the dice color based on the User's selection
+        if (diceColor === "white") {
+        //Show die depending on the size the user chooses
+            if (diceSize === 4) {
+                document.getElementById("d4ImageLight").style.visibility = "visible";
+            } else if (diceSize === 6) {
+                document.getElementById("d6ImageLight").style.visibility = "visible";
+            } else if (diceSize === 8) {
+                document.getElementById("d8ImageLight").style.visibility = "visible";
+            } else if (diceSize === 10) {
+                document.getElementById("d10ImageLight").style.visibility = "visible";
+            } else if (diceSize === 12) {
+                document.getElementById("d12ImageLight").style.visibility = "visible";
+            } else if (diceSize === 20) {
+                document.getElementById("d20ImageLight").style.visibility = "visible";
+            }
+        } else if (diceColor === "black") {
+            //Show die depending on the size the user chooses
+            if (diceSize === 4) {
+                document.getElementById("d4ImageDark").style.visibility = "visible";
+            } else if (diceSize === 6) {
+                document.getElementById("d6ImageDark").style.visibility = "visible";
+            } else if (diceSize === 8) {
+                document.getElementById("d8ImageDark").style.visibility = "visible";
+            } else if (diceSize === 10) {
+                document.getElementById("d10ImageDark").style.visibility = "visible";
+            } else if (diceSize === 12) {
+                document.getElementById("d12ImageDark").style.visibility = "visible";
+            } else if (diceSize === 20) {
+                document.getElementById("d20ImageDark").style.visibility = "visible";
+            }
+        }
+
+    }, 2000 * i);
+}
+
+
 }
 
 
