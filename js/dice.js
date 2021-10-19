dbf86c0a0ead20f93560d06a199d7dcd82a4eea5
alillo2 <alillo2@student.cvtc.edu>
2021-10-19 17:51:47 -0500

    [Finishes #179956894] Created a tutorial video

--- js/dice.js
+++ js/dice.js
@@ -194,4 +194,13 @@ function normalRoll(i) {
 }
 
 
+function loadVideo(id)
+{
+    var video = document.getElementById('video');
+    var mp4 = document.getElementById('mp4');
 
+    mp4.src = "Video/" + id;
+
+    video.load();
+    video.play();
+}
