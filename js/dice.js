9d37b062bbc491ec5d1956b80afcfd6902538bea
Jacob Frideres <jakefrideres@gmail.com>
2021-10-19 12:12:47 -0500

    Fixed backgroundSound and gave it it's own file.

--- js/dice.js
+++ js/dice.js
@@ -4,39 +4,12 @@
 var totalSum = 0;
 var rollSound = new Audio();
 var crashSound = new Audio();
-var volume = "";
-var backgroundAudio = "";
-var backgroundVolumeSlider = "";
-
 
 //Determines the volume of the roll sound
 volume = document.getElementById("volume-control");
 volume.addEventListener("change", function(e) {
 rollSound.volume = e.currentTarget.value / 100;
 });
-
-/*  
-Credits for the background music
-
-Slow Grind by Ron Gelinas Chillout Lounge | https://soundcloud.com/atmospheric-music-portal
-Creative Commons Attribution 3.0 Unported License
-https://creativecommons.org/licenses/by/3.0/deed.en_US
-Music promoted by https://www.chosic.com/free-music/all/
-*/
-
-//Plays the background music as soon as someone opens the diceroll page
-backgroundAudio = new Audio();
-backgroundAudio.src = "audio/ron-gelinas-chillout-lounge-slow-grind.mp3";
-backgroundAudio.play();
-
-//Allows the user to change the volume of the background music
-backgroundVolumeSlider = document.getElementById("backgroundVolume-control");
-backgroundVolumeSlider.addEventListener("mousemove", setBackgroundVolume);
- 
-function setBackgroundVolume() {
-    backgroundAudio.volume = backgroundVolumeSlider.value / 100;
-}
-
 
 function rollDice() {
     "use strict";
