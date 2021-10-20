c4198f8d9f4004bc9704e9bb882b7c5ce935cf18
Elliot Dubrow <emn2345@gmail.com>
2021-10-20 12:53:25 -0500

    [Delivers #179692934] finished login screen

---
+++ js/cookiescript.js
@@ -0,0 +1,31 @@
+function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+    
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return cookiePair[1];
+        }
+    }
+    // Return null if not found
+    return null;
+}
+
+function checkCookie() {
+    // Get cookie using our custom function
+
+    
+    var userName = getCookie("username");
+    console.log(userName);
+    var cookieText = document.getElementById('usernameText');
+
+    cookieText.innerHTML = userName;
+    }
+
+checkCookie();
--- js/userlogin.js
+++ js/userlogin.js
@@ -6,14 +6,10 @@ function userLogin() {
     "use strict";
 
     var username = document.getElementById('userUsername').value;
-    var password = document.getElementById('userPassword').value;
 
     var currentWindow = window;
 
-    if (username.length >= 6 && password.length >= 6) {
+        //setting cookie for the browsing session
+        document.cookie = "username=" + username;
         currentWindow.location.href = "diceroll.html";
-    } else {
-        window.alert("Please make sure that the username and password you have entered are at least 6 characters long.");
-    }
-
 }
